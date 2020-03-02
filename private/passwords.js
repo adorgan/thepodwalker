c734e51370a597f6d0f99496d8f0bd6a5aa541c0
adorg <adorgan@gmail.com>
2020-03-01 20:04:36 -0600

    updated passwords

--- private/passwords.js
+++ private/passwords.js
@@ -1,5 +1,5 @@
 module.exports.passwordMongoDB = "mongodb+srv://andrew:Bonjovi1@cluster0-xt2mp.mongodb.net/test?retryWrites=true&w=majority";
 
-module.exports.passwordGmail = "illini!(52";
+module.exports.passwordGmail = "yiEvPpivd95EbD4";
 
 module.exports.passwordSession = "Bear is the best and cutest dog in the world."
