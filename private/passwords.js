714ee38aba8adc9ec5e42322e8343fd6b385faea
Andrew Dorgan <dorgana@oregonstate.edu>
2020-04-10 07:57:19 -0500

    updated privates

--- private/passwords.js
+++ private/passwords.js
@@ -1,5 +1,5 @@
 module.exports.passwordMongoDB = "mongodb+srv://andrew:Bonjovi1@cluster0-xt2mp.mongodb.net/test?retryWrites=true&w=majority";
 
-module.exports.passwordGmail = "yiEvPpivd95EbD4";
+module.exports.passwordGmail = "owwwJiGof*&s2kk3";
 
 module.exports.passwordSession = "Bear is the best and cutest dog in the world."
