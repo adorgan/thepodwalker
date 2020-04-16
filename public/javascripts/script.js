ac1a48afe994ae2574c556930d817ca3a2d32f90
Andrew Dorgan <adorgan@gmail.com>
2020-04-15 20:41:51 -0500

    changed carousel pic height

---
+++ public/javascripts/script.js
@@ -0,0 +1,16 @@
+// var images;
+
+// $(function() {  
+//     images = $('img-car');
+//     removeSize();
+// });
+
+// $(window).resize(removeResize);
+
+// function removeSize()
+// {
+//     if($(window).width() < 600 && images.length > 0)
+//     {
+//         images.removeAttr('height');
+//     }
+// }
