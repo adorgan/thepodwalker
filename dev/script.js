616689124275b85b6fd448851c6f0970d7fb00ec
Andrew Dorgan <adorgan@gmail.com>
2020-04-17 18:03:56 -0500

    restyled slideshow

--- dev/script.js
+++ dev/script.js
@@ -1,11 +1,3 @@
-function openModal() {
-  document.getElementById("myModal").style.display = "block";
-}
-
-function closeModal() {
-  document.getElementById("myModal").style.display = "none";
-}
-
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -20,17 +12,15 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
+  var images = document.getElementsByTagName("img");
+  
+  if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+      slides[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  
+  slides[slideIndex-1].style.display = "block";  
+  var caption = document.getElementById("caption");
+  caption.innerHTML = images[slideIndex-1].alt;
 }
