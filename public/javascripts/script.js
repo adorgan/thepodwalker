f0bf8c1dc49e070c60a705810ff3f409fad73e93
Andrew Dorgan <adorgan@gmail.com>
2020-04-28 17:05:36 -0500

    fixed caption text changing on resize

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,8 +1,12 @@
+var slides = document.getElementsByClassName("mySlides");
+var images = document.getElementsByTagName("img");
+
 var imageAlts = document.getElementsByTagName("img");
 var alts = [];
 
 for(var i = 0; i < imageAlts.length; i++){
     alts[i] = imageAlts[i].alt;
+    
 }
 
 
@@ -15,18 +19,21 @@ for(var i = 0; i < imageAlts.length; i++){
     }
 }
 
+var slideIndex = 1;
 
 var divComment = document.getElementById("caption-container");
 var caption = document.getElementById("caption");
 
 function calcCommentHeight(){
+  
     caption.innerHTML = longest;
     divComment.style.height = caption.offsetHeight + 12 ;
+    caption.innerHTML = images[slideIndex-1].alt;
 }
 calcCommentHeight();
 
 
-var slideIndex = 1;
+
 showSlides(slideIndex);
 
 slideFadeCount = 0;
@@ -48,8 +55,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var images = document.getElementsByTagName("img");
+  
   
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
