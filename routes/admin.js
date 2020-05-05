05532f7931dee3c4f05487441779ff275cfe3068
Andrew Dorgan <adorgan@gmail.com>
2020-05-05 07:37:05 -0500

    uncommented email

--- routes/admin.js
+++ routes/admin.js
@@ -38,40 +38,41 @@ router.get("/admin/episodes/new", isLoggedIn, function (req, res) {
 
 //update database with new episode, send mailer to email list
 router.post("/admin/episodes/new", function (req, res) {
-    Blogpost.create(req.body.blog, function (err, newBlog) {
+    console.log(req.body.blog);
+    Blog.create(req.body.blog, function (err, newBlog) {
         if (err) {
             res.render("new");
         }
         else {
-        //     if(req.body.emailCheck == "on"){
-        //         console.log("check works");
+            if(req.body.emailCheck == "on"){
+                console.log("check works");
             
-        //     Email.find({}, function (err, emailArray) {
-        //         emailArray.forEach(function (email) {
-        //             setTimeout(function(){}, 2000);
-        //             var mailOptions = {
-        //                 from: 'thepodwalker@gmail.com',
-        //                 to: email.email,
-        //                 subject: 'New Pod Walker Episode',
-        //                 html:   "<div style='color:black;'>Hey "+email.firstName+",</div>"+
-        //                         "<div style='color: black'>Check out the newest episode of the Pod Walker below.</div><br>"+
-        //                         "<div><a href='https://thepodwalker.com/episodes/" + newBlog._id+"'style='padding: 5px; text-decoration:none;color:black;'>"+
-        //                         "<div>"+newBlog.episodeNum+"</div>"+ 
-        //                         "<div><strong>" + newBlog.title + "</strong></div>"+
-        //                         "<img style='width:350px;margin-top:0px;padding-top:0px;'src="+newBlog.image[0]+"></a></div>"+
-        //                         "<br><br><br>"+
-        //                         "<div><a style='text-decoration:none;color:blue;' href='https://www.thepodwalker.com/unsubscribe'>Unsubscribe</a></div>"
-        //             };
-        //             transporter.sendMail(mailOptions, function (error, info) {
-        //                 if (error) {
-        //                     console.log(error);
-        //                 } else {
-        //                     console.log('Email sent: ' + info.response);
-        //                 }
-        //             });
-        //         });
-        //     });
-        // }
+            Email.find({}, function (err, emailArray) {
+                emailArray.forEach(function (email) {
+                    setTimeout(function(){}, 2000);
+                    var mailOptions = {
+                        from: 'thepodwalker@gmail.com',
+                        to: email.email,
+                        subject: 'New Pod Walker Episode',
+                        html:   "<div style='color:black;'>Hey "+email.firstName+",</div>"+
+                                "<div style='color: black'>Check out the newest episode of the Pod Walker below.</div><br>"+
+                                "<div><a href='https://thepodwalker.com/episodes/" + newBlog._id+"'style='padding: 5px; text-decoration:none;color:black;'>"+
+                                "<div>"+newBlog.episodeNum+"</div>"+ 
+                                "<div><strong>" + newBlog.title + "</strong></div>"+
+                                "<img style='width:350px;margin-top:0px;padding-top:0px;'src="+newBlog.image[0]+"></a></div>"+
+                                "<br><br><br>"+
+                                "<div><a style='text-decoration:none;color:blue;' href='https://www.thepodwalker.com/unsubscribe'>Unsubscribe</a></div>"
+                    };
+                    transporter.sendMail(mailOptions, function (error, info) {
+                        if (error) {
+                            console.log(error);
+                        } else {
+                            console.log('Email sent: ' + info.response);
+                        }
+                    });
+                });
+            });
+        }
             
             res.redirect("/admin/episodes");
         }
